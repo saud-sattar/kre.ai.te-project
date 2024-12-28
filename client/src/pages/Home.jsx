5b0732d84caf8c3892799b628a390b1dcb72bbe9
Saud Sattar <83525131+saud-sattar@users.noreply.github.com>
2024-12-29 09:48:08 +1100

    changed the url after deploying backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,12 +26,15 @@ const Home = () => {
          setLoading(true);
 
          try {
-            const response = await fetch("http://localhost:8080/api/v1/post", {
-               method: "GET",
-               headers: {
-                  "Content-Type": "application/json",
-               },
-            });
+            const response = await fetch(
+               "https://kre-ai-te-project.onrender.com/api/v1/post",
+               {
+                  method: "GET",
+                  headers: {
+                     "Content-Type": "application/json",
+                  },
+               }
+            );
 
             if (response.ok) {
                const result = await response.json();
